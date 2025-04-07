121b2ab18d54699c361c82148a3976e923bc23f4
Blake Cromar <blake.cromar@icloud.com>
2025-04-07 15:59:25 +0200

    Changed the duration of countdowns

--- azure-ml-deployment/delete-env.sh
+++ azure-ml-deployment/delete-env.sh
@@ -57,7 +57,7 @@ fi
 # ------------------------------------------------------------------------------
 echo "⏳ Waiting for Key Vault to become available for purge: $KEY_VAULT_NAME"
 
-retry_with_countdown 12 10 "az keyvault purge --name \"$KEY_VAULT_NAME\""
+retry_with_countdown 6 5 "az keyvault purge --name \"$KEY_VAULT_NAME\""
 
 
 # ------------------------------------------------------------------------------
